451f2116cf78552c622a59aeaa6f6df8db96b9e3
Gustavo <luis.gustavo09@hotmail.com>
2024-06-25 14:20:55 -0400

    fix: resolve compilation errors in package.json and tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { routes } from './routes'
 const server = fastify({ logger: false })
 
 server.register(routes, { prefix: '/api/v1' })
-server.listen({ port: 3000 }, (err, address) => { err ? console.error(err) : console.log(`Server listening at ${address}`) })
+server.listen({ port: 3045 }, (err, address) => { err ? console.error(err) : console.log(`🚀 Servidor rodando no endereço ${address}`) })
